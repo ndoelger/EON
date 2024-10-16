415ddd83ed600ab7241da6140e18ddfbc1841860
Nic Doelger <doelgern@gmail.com>
2024-10-16 18:21:42 -0400

    2 buttons

---
+++ App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+export default function App() {
+  const [mood, setMood] = useState('');
+
+  return (
+    <View style={styles.appContainer}>
+      <Pressable
+        style={{
+          ...styles.pressable,
+          backgroundColor: mood === 'Exhausted' ? 'green' : 'white',
+        }}
+        onPress={() => {
+          setMood('Exhausted');
+        }}
+      >
+        <Text style={styles.text}>Exhausted</Text>
+      </Pressable>
+      <Pressable
+        style={{
+          ...styles.pressable,
+          backgroundColor: mood === 'Nauseous' ? 'green' : 'white',
+        }}
+        onPress={() => {
+          setMood('Nauseous');
+        }}
+      >
+        <Text style={styles.text}>Nauseous</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    margin: 'auto',
+    padding: 50,
+    paddingHorizontal: 16,
+    marginTop: 20,
+    height: '100%',
+    gap: 20,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50,
+    textAlign: 'center',
+    padding: 10,
+  },
+  pressable: {
+    borderWidth: 2,
+  },
+});
